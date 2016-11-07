4266fb8b4b44e08b84ccea070a1f52ee27e98f12
Tyler Nunez <tyler.nunez@selu.edu>
2016-11-07 14:01:23 -0600

    Added Moving platforms, still an issue when landing on platform, seems to transform the position of the camera.

---
+++ Assets/Scripts/platformMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformMovement : MonoBehaviour {
+
+	public float speed = 6.0F;
+	private int direction = 1;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		transform.Translate(Vector3.forward * speed * direction * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Target")
+		{
+			if(direction == 1)
+				direction = -1;
+			else
+				direction = 1;
+		}
+		if(other.tag == "Player")
+			other.transform.parent = transform;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			other.transform.parent = null;
+		}
+	}
+}
