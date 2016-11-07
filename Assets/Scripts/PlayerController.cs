1552d4abeef5131ac6869e2e0b52d16a97d6a81c
Tyler Nunez <W0532191@selu.edu>
2016-11-07 07:31:09 -0600

    Initial Commit, includes Unity project with current work so far. I want to create a superior platforming player controller as my goal next.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 6.0F;
+    public float jumpHeight = 6.0F;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        float verticalMovement = Input.GetAxis("Vertical") * speed;
+        float horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        verticalMovement *= Time.deltaTime;
+        horizontalMovement *= Time.deltaTime;
+
+        transform.Translate(horizontalMovement, 0, verticalMovement);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+        if (Input.GetKeyDown("space"))
+            rb.AddForce(Vector3.up * jumpHeight);
+    }
+}
+
